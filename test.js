37839fd827391598cb4ef68dd77764db6cd6254b
Julian Gruber <julian@juliangruber.com>
2014-05-17 10:01:04 -0700

    add set after delete test + refactor

--- test.js
+++ test.js
@@ -10,29 +10,43 @@ describe('clone(obj)', function(){
     deepEqual(cloned, obj);
   });
 
-  it('should add a value', function(){
-    var obj = { foo: 'bar' };
-    var cloned = clone(obj);
-    cloned.bar = 'baz';
-    deepEqual(cloned, { foo: 'bar', bar: 'baz' });
-    deepEqual(obj, { foo: 'bar' });
+  describe('set', function(){
+    it('should add a value', function(){
+      var obj = { foo: 'bar' };
+      var cloned = clone(obj);
+      cloned.bar = 'baz';
+      deepEqual(cloned, { foo: 'bar', bar: 'baz' });
+      deepEqual(obj, { foo: 'bar' });
+    });
+  
+    it('should override a value', function(){
+      var obj = { foo: 'bar' };
+      var cloned = clone(obj);
+      cloned.foo = 'baz';
+      deepEqual(cloned, { foo: 'baz' });
+      deepEqual(obj, { foo: 'bar' });
+    });
+    
+    it('should add a previously deleted value', function(){
+      var obj = { foo: 'bar' };
+      var cloned = clone(obj);
+      delete cloned.foo;
+      cloned.foo = 'baz';
+      deepEqual(cloned, { foo: 'baz' });
+      deepEqual(obj, { foo: 'bar' });
+    });
   });
 
-  it('should override a value', function(){
-    var obj = { foo: 'bar' };
-    var cloned = clone(obj);
-    cloned.foo = 'baz';
-    deepEqual(cloned, { foo: 'baz' });
-    deepEqual(obj, { foo: 'bar' });
+  describe('delete', function(){
+    it('should delete a value', function(){
+      var obj = { foo: 'bar' };
+      var cloned = clone(obj);
+      delete cloned.foo;
+      delete cloned.unknown;
+      deepEqual(cloned, {});
+      deepEqual(obj, { foo: 'bar' });
+    });
   });
 
-  it('should delete a value', function(){
-    var obj = { foo: 'bar' };
-    var cloned = clone(obj);
-    delete cloned.foo;
-    delete cloned.unknown;
-    deepEqual(cloned, {});
-    deepEqual(obj, { foo: 'bar' });
-  });
 });
 
